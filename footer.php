5d194b7cb2559c4ed9949a27efb692c3a8bb98cd
Khalid <dev.y2mi@gmail.com>
2023-12-07 00:58:54 +0300

    index.html -> index.php + style + header/footer php

---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer>
+    <div class="general_info">
+        <div class="legainaha_info">
+            <a href="#">
+                <img src="./assets/images/grey logo.svg">
+                <img src="./assets/images/legainaha.svg">
+            </a>
+            <p>
+                <strong>لِقيناها منصــة إلـكتــرونـيـــه مُعتمـــدة</strong>
+                <br>
+                لخدمات ما بعد البيع شاملة لكل ما تحتاجه
+                <br>
+                السياره من قطع الغبار والإطارات والزيوت
+                <br>
+                وغيرها من إحتياجات السيارة من خلال
+                <br>
+                الربط التقني المباشر بين المستخدم وبين
+                <br>
+                شركائنا حول المملكة.
+            </p>
+        </div>
+        <div class="contact">
+            <p>
+                <strong>تواصل معنا</strong>
+            </p>
+            <div id="tel">
+                <img src="./assets/images/call.svg">
+                <a href="tel:0544090264" style="color: black; margin-right: 10px">800xxxxxxx</a>
+            </div>
+            <br>
+            <div id="email">
+                <img src="./assets/images/email.svg">
+                <a href="mailto:care@legainaha.com" style="color: black; margin-right: 10px;">care@Legainaha.com</a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html lang="ar" dir="rtl">
+    <head>
+        <link rel="stylesheet" href="./styles/land_style.css?v=<?php echo time();?>">
+        <title>
+            Landing page
+        </title>
+        
+    </head>
+
+    <body>
+        <?php include('header.php');?>
+        <h1 id="page-title">أهلاً بك في لِقيناها!</h1>
+        <div class="offers-wrapper">
+            <img id="offer1" src="./assets/offers/offer1.png" alt="offer 1">
+            <img id="selected-img" src="./assets/offers/offer1.png" alt="offer 2">
+            <img id="offer3" src="./assets/offers/offer1.png" alt="offer 3">
+        </div>
+
+        <div class="app-sections">
+            <div class="sections-title"><h2>تصفح جميع الأقسام</h2></div>
+            <a href="./pages/catigoryResults.html" target="_self"><div class="white-circle"><img src="./assets/sections/offers.png" alt="offers-section"> <h4>العروض</h4></div></a>
+            <a href="./pages/catigoryResults.html" target="_self"><div class="white-circle"><img src="./assets/sections/brands.png" alt="offers-section"> <h4>الماركات</h4></div></a>
+            <a href="./pages/catigoryResults.html" target="_self"><div class="white-circle"><img src="./assets/sections/tyres.png" alt="offers-section"> <h4>الكفرات</h4></div></a>
+            <a href="./pages/catigoryResults.html" target="_self"><div class="white-circle"><img class="mech-pic" src="./assets/sections/mech-parts.png" alt="offers-section"> <h4>القطع الميكانيكية</h4></div></a>
+            <a href="./pages/catigoryResults.html" target="_self"><div class="white-circle"><img src="./assets/sections/car-bodies.png" alt="offers-section"> <h4>البودي</h4></div></a>
+            <a href="./pages/catigoryResults.html" target="_self"><div class="white-circle"><img src="./assets/sections/oils.png" alt="offers-section"> <h4>الزيوت</h4></div></a>
+            <a href="./pages/catigoryResults.html" target="_self"><div class="white-circle"><img src="./assets/sections/cons-parts.png" alt="offers-section"> <h4>القطع الإستهلاكية</h4></div></a>
+            <a href="./pages/catigoryResults.html" target="_self"><div class="white-circle"><img src="./assets/sections/elec-parts.png" alt="offers-section"> <h4>القطع الكهربائية</h4></div></a>
+        </div>
+
+        <?php include('footer.php');?>
+        <script src="./scripts/land_script.js"></script>
+    </body>
+</html>
