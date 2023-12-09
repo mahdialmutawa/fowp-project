649e1b7f99562cf93d6b34b8273f48c2c211d0d1
Khalid <dev.y2mi@gmail.com>
2023-12-09 07:31:00 +0300

    sign-up / order stat updates

--- pages/Signup.php
+++ pages/Signup.php
@@ -7,6 +7,7 @@
     <title>
         Sign up
     </title>
+    
 </head>
 
 <body>
@@ -19,18 +20,16 @@
 
         <nav>
             <div class="navigation_links">
-
-                </li>
                 </ul>
             </div>
         </nav>
     </header>
 
     <div class="container">
-        <div class="signup-box" >
+        <div class="signup-box">
             <h2>اهلا بك في لِقيناها</h2>
-            
-            <form action="./php/sign-up.php" method="POST" id="sign-up-form" name="new-cust" >
+
+            <form action="./php/sign-up.php" method="POST" id="sign-up-form" name="new-cust">
                 <div class="inputF">
                     <input type="text" placeholder="الاسم الاول " required id="fname" name="fname"><br>
                 </div>
@@ -46,58 +45,73 @@
                 <div class="inputF">
                     <input type="password" placeholder="تأكيد كلمة المرور" required id="PasswordConfirm" name="conpwd"><br>
                 </div>
-
-                <input type="submit" name="test" value="إنشاء حساب">
-
+                <div class="inputF">
+                    <select id="securityQuestion" name="securityQuestion" required>
+                        <option value="" disabled selected>اختر سؤال الأمان</option>
+                        <option value="q1">ما هو اسم أول معلم لك؟</option>
+                        <option value="q2">ما هو اسم حيوانك المفضل؟</option>
+                        <option value="q3">ما هي مدينتك الأم؟</option>
+                    </select>
+                </div>
+                <div class="inputF">
+                    <input type="text" placeholder="إجابة سؤال الأمان" required id="Q" name="securityAnswer"><br>
+                </div>
+                <input type="submit" name="test" value="إنشاء حساب" id = "Ans">
             </form>
 
         </div>
-
     </div>
 
+    <script>
+        var Create = document.getElementById("createButton");
 
+        Create.addEventListener("click", ButtonClicked);
+
+        function ButtonClicked() 
+        {
+            var password1 = document.getElementById("Password").value;
+            var password2 = document.getElementById("PasswordConfirm").value;
+
+            let fname = document.getElementById("fname").value.trim();
+            let email = document.getElementById("email").value.trim();
+            let pnum = document.getElementById("pnum").value.trim();
+            let Q = document.getElementById("Q").value.trim();
+            let Ans = document.getElementById("Ans").value.trim();
+
+            if (password1 === password2 && fname && email && pnum && password1 && password2) 
+            {
+                event.preventDefault(); 
+            }
+            else 
+            {
+                if (password1 !== password2) 
+                {
+                    alert("Passwords do not match. Please make sure your passwords match.");
+                } 
+                else if (!fname) 
+                {
+                    alert("Please enter your first name.");
+                } 
+                else if (!email) 
+                {
+                    alert("Please enter your email address.");
+                }
+                 else if (!pnum)
+                {
+                    alert("Please enter your phone number.");
+                }
+                else if (!Q)
+                {
+                    alert("Please Security question.");
+                }
+                else if (!Ans)
+                {
+                    alert("Please enter your answer.");
+                }
+            }
+        }
+    </script>
 
 </body>
-
-
-<script>
-    var Create = document.getElementById("createButton");
-
-    Create.addEventListener("click", ButtonClicked);
-
-    function ButtonClicked() 
-    {
-        var password1 = document.getElementById("Password").value;
-        var password2 = document.getElementById("PasswordConfirm").value;
-
-        let fname = document.getElementById("fname").value.trim();
-        let email = document.getElementById("email").value.trim();
-        let pnum = document.getElementById("pnum").value.trim();
-
-        if (password1 === password2 && fname && email && pnum && password1 && password2) 
-        {
-            event.preventDefault(); 
-        }
-        else 
-        {
-            if (password1 !== password2) 
-            {
-                alert("Passwords do not match. Please make sure your passwords match.");
-            } 
-            else if (!fname) 
-            {
-                alert("Please enter your first name.");
-            } 
-            else if (!email) 
-            {
-                alert("Please enter your email address.");
-            }
-             else if (!pnum)
-            {
-                alert("Please enter your phone number.");
-            }
-        }
-    }
-</script>
 
 </html>
